c70f19d79d2a9c0fab4d007e6e81729a10a49d52
raninder dhillon <raninderdhillon@gmail.com>
2022-02-07 21:36:08 -0500

    printborder and frame around text

---
+++ printInFrame.js
@@ -0,0 +1,50 @@
+// *********
+// * May   *
+// * the   *
+// * force *
+// * be    *
+// * with  *
+// * you   *
+// *********
+
+const printInFrame = function(list) {
+  list = list.split(' ');
+	console.log("list",list);
+  const longest = longestStr(list).length;
+	console.log("longest",longest);
+	
+  const border = repeat('*', longest);
+
+  console.log(border);
+  for (const word of list) {
+    console.log(`* ${word}${repeat(' ', longest - word.length + 1)}*`);
+  }
+  console.log(border);
+};
+
+const repeat = function(str, times) {
+  let result = str;
+
+  for (let i = 0; i < times; i++) {
+    result += str;
+  }
+
+  return result;
+};
+
+const longestStr = function(list) {
+  let longest = list[0];
+
+  for (const str of list) {
+		if(str.length>longest.length)
+    longest = str;
+  }
+
+  return longest;
+};
+
+// Test driver code, do not modify
+printInFrame('May the force be with you');
+printInFrame('Here\'s Johnny!');
+printInFrame('Supercalifragalisticexpialadocious');
+printInFrame('Lost, like tears in the rain');
